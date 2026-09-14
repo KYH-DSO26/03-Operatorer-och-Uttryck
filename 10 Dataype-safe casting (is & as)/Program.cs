763879b4ff7161f7561f167cce9cdea4bcca9daa
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 13:38:51 +0300

    Efter uppgift 10

---
+++ 10 Dataype-safe casting (is & as)/Program.cs
@@ -0,0 +1,15 @@
+﻿object input1 = "Hej Båten!";
+string? text1 = input1 as string; // Säker typkonvertering med "as"
+Console.WriteLine($"Resultat av as: {text1 ?? "Inte en sträng"}");
+
+object input2 = 42;
+string? text2 = input2 as string; // Säker typkonvertering med "as"
+Console.WriteLine($"Resultatet av as vid fel typ: {text2 ?? "Detta var inte en sträng!"}");
+
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
+
+
