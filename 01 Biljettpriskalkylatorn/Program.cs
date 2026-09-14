278fc06fe068d1c143b3970876fdb6c99e18c1bc
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 11:11:09 +0300

    Efter uppgift 1

--- 01 Biljettpriskalkylatorn/Program.cs
+++ 01 Biljettpriskalkylatorn/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿decimal grundPris = 120.00m;
+int antal = 3;
+decimal totalPris = grundPris * antal;
+
+if (antal >= 3)
+{
+    //totalPris = totalPris * 0.90m;  // 10% rabatt är samma sak som att multiplicera med 0.90
+    totalPris *= 0.90m;  // 10% rabatt är samma sak som att multiplicera med 0.90
+
+    //decimal rabatt = grundPris * antal * 0.10m;  // Beräkna rabatten
+    //totalPris = totalPris - rabatt;  // Dra av rabatten från totalpriset
+    //totalPris -= rabatt;  // Dra av rabatten från totalpriset
+}
+
+Console.WriteLine($"Det rabatterade priset är {totalPris:F2} kr.");
+
+
+
+Console.Write("Tryck på valfri tangent för att avsluta...");
+Console.ReadKey();
