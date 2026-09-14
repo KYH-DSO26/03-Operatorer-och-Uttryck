b0c4dee3cc2f68209573dc15db55d053f3e5a1c3
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 12:04:21 +0300

    Efter uppgift 4

---
+++ 04 Godisdelaren/Program.cs
@@ -0,0 +1,13 @@
+﻿int godisar = 23;
+int vänner = 4;
+
+int perPerson = godisar / vänner;
+int över = godisar % vänner;
+
+Console.WriteLine($"Varje person får {perPerson} godisar. Det blir {över} kvar i påsen");
+
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
