f2b73bf88a2ad8fe1ef5acb0001b60c045a0b0be
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 11:44:44 +0300

    Efter uppdaterad uppgift 2

--- 02 Ålderskontrollen/Program.cs
+++ 02 Ålderskontrollen/Program.cs
@@ -1,9 +1,19 @@
 ﻿//Console.WriteLine("02 Ålderskontrollen");
 //Console.WriteLine("======================");
+
+
+const int myndighetsÅlder = 18;
 Console.Write("Hur gammal är du?: ");
-int ålder = int.Parse(Console.ReadLine()!);
-bool ärMyndig = ålder >= 18;
-Console.WriteLine($"Du är myndig: {ärMyndig}");
+bool ärMyndig = int.TryParse(Console.ReadLine(), out int ålder) && ålder >= myndighetsÅlder;
+string svar = ärMyndig ? "Du är myndig." : "Du är inte myndig.";
+Console.WriteLine(svar);
+
+
+
+
+//int ålder = int.Parse(Console.ReadLine()!);
+//bool ärMyndig = ålder >= 18;
+//Console.WriteLine($"Du är myndig: {ärMyndig}");
 //Console.WriteLine("======================");
 
 
