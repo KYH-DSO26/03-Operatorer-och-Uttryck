307617378dfafb92897d181b098592f54186bd0f
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 12:42:53 +0300

    Efter uppgift 6

---
+++ 06 Medlemsrabatt/Program.cs
@@ -0,0 +1,17 @@
+﻿bool ärGuldMedlem = false;
+bool ärSenior = true;
+decimal köpBelopp = 550.00m;
+
+
+// && har högre precedens än ||, så det är viktigt att använda parenteser för att
+// tydliggöra logiken om det behövs.
+// I detta fall är det dock korrekt som det är.
+bool fårRabatt = ärGuldMedlem || (ärSenior && köpBelopp > 500m);
+
+Console.WriteLine($"Får rabatt: {fårRabatt}");
+
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
