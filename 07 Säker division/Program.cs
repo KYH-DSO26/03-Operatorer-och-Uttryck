694b25322676242ea671086d06a3caf9fa49cd5d
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 12:56:56 +0300

    Efter uppgift 7

---
+++ 07 Säker division/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Mata in TÄLJARE: ");
+double täljare = double.Parse(Console.ReadLine()!);
+Console.Write("Mata in NÄMNARE: ");
+double nämnare = double.Parse(Console.ReadLine()!);
+
+double resultat = (nämnare != 0) ? täljare / nämnare : 0;
+
+string svar = (nämnare == 0) ? "Fel: kan inte dela med noll! Resultatet satt till 0" : $"Resultat: {resultat}";
+Console.WriteLine(svar);
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
