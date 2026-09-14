a2263335660a16dbaa81b4d3477edc506609d255
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 13:04:20 +0300

    Efter uppgift 8

---
+++ 08 Stränglängds-kontroll/Program.cs
@@ -0,0 +1,16 @@
+﻿string? text = null;
+
+int längd = text?.Length ?? 0;  // Om text är null, blir text?.Length null
+                                // och ?? operatorn sätter längd till 0
+
+Console.WriteLine($"Strängens längd är: {längd}");
+
+text = "Claes";
+längd = text?.Length ?? 0;  // Om text är null, blir text?.Length null
+                            // och ?? operatorn sätter längd till 0
+
+Console.WriteLine($"Strängen: {text} längd är: {längd}");
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
