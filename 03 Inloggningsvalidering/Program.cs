69225ed334b9b1480a6781f1c779f14fc5fe84ec
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 11:55:47 +0300

    Efter uppgift 3

---
+++ 03 Inloggningsvalidering/Program.cs
@@ -0,0 +1,16 @@
+﻿string korrektAnvändarnamn = "admin";
+string korrektLösenord = "hemligt";
+
+Console.Write("Ange användarnamn: ");
+string användarnamn = Console.ReadLine()!;
+Console.Write("Ange lösenord: ");
+string lösenord = Console.ReadLine()!;
+
+bool inloggad = ((användarnamn == korrektAnvändarnamn) && (lösenord == korrektLösenord));
+Console.WriteLine($"Inloggningen lyckades: {inloggad}");
+
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
