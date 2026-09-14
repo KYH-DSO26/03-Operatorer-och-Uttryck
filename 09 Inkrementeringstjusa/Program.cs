2bd1c4681d12836782d9a62bd6028a7a88205021
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 13:16:26 +0300

    Efter uppgift 9

---
+++ 09 Inkrementeringstjusa/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5;
+int b = 5;
+
+Console.WriteLine($"Prefix (++a): {++a}");  // Ökar först, skriver sedan ut värdet
+Console.WriteLine($"Postfix (b++): {b++}"); // Skriver först ut värdet, ökar sedan
+
+Console.WriteLine($"Värdet på b efteråt: {b}");
+
+Console.WriteLine($"\n\nPrefix (--a): {--a}");  // Minskar först, skriver sedan ut värdet
+Console.WriteLine($"Postfix (b--): {b--}"); // Skriver först ut värdet, minskar sedan
+
+Console.WriteLine($"Värdet på b efteråt: {b}");
+
+
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
