53ada469827f85d1ecb24e5ab02aef84b8745cfc
Claes Engelin <claes.engelin@zocom.se>
2026-09-14 12:10:49 +0300

    Efter uppgift 5

---
+++ 05 Implicit vs Explicit Typkonvertering/Program.cs
@@ -0,0 +1,12 @@
+﻿int tal = 500;
+double implicitTal = tal; // Implicit konvertering från int till double
+double stortTal = 123.45;
+int explicitTal = (int)stortTal; // Explicit konvertering från double till int
+
+Console.WriteLine($"Implicit double: {implicitTal}");
+Console.WriteLine($"Explicit double (kapad): {explicitTal}");
+
+
+
+Console.WriteLine("\nTryck på en tangent för att avsluta...");
+Console.ReadKey();
